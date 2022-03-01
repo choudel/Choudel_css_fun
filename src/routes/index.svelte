723d4874a78318eba24bc1b50b6490901fc91220
choudel <chouaibh@gmail.com>
2022-03-01 21:55:10 +0100

    accordion with action

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,21 @@
 <script>
+	import Baccordion from './lib/baccordion.svelte';
+	import Accordion from './lib/accordion.svelte';
 	import Toggle from './lib/toggle.svelte';
 	let isToggled = false;
 </script>
 
+
+
 <h1>Welcome to compo_test</h1>
+<Accordion isOpen ={false} buttonText="acoordion">
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam fugit veritatis voluptate sunt rerum totam ex vero eveniet iste ducimus, eos, facilis laborum sit non cumque nam, est minus neque?</p>
+</Accordion>
+<Baccordion isOpen ={true} buttonText="beta accordion">
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam fugit veritatis voluptate sunt rerum totam ex vero eveniet iste ducimus, eos, facilis laborum sit non cumque nam, est minus neque?</p>
+</Baccordion>
+
+
 <Toggle bind:isToggled label="BEta" />
 <Toggle bind:isToggled label="Sigma" style="--backgroudChecked: grey;" />
 {#if isToggled}
---
+++ src/routes/lib/baccordion.svelte
@@ -0,0 +1,41 @@
+<script>
+	import {slide} from './slide';
+	export let buttonText = '';
+	export let isOpen = true;
+</script>
+
+<button on:click={() => (isOpen = !isOpen)}>
+	<span class:isOpen>A</span>
+	{buttonText}</button
+>
+
+<div class="accordion-content" use:slide={isOpen} on:animationEnd={()=>console.log('endeddd')}>
+	<div class="wrapper"><slot /></div>
+</div>
+
+<style>
+	button {
+		display: block;
+		border: 0;
+		width: 100%;
+		text-align: left;
+		border: solid 1px #333;
+		margin: 0;
+	}
+	span {
+		margin-right: 5px;
+		display: inline-block;
+		transform: rotate(0.25turn);
+		transition: transform 0.5s ease;
+	}
+	.isOpen {
+		transform: rotate(0.5turn);
+	}
+	.accordion-content {
+		border: solid 1px #333;
+		border-top: 0;
+	}
+	.wrapper{
+		padding: 20px;
+	}
+</style>
