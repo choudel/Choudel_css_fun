648e2876b60781dc4fb7a3652604c5d28448be5d
choudel <chouaibh@gmail.com>
2022-05-13 06:47:38 +0100

    add mobile breakpoint and transitions

--- src/routes/lib/timeline.svelte
+++ src/routes/lib/timeline.svelte
@@ -1,10 +1,12 @@
 <script>
+	import { fade } from 'svelte/transition';
 	let timeline = [
-		{ date: '01/04/2022', h2: 'test left text', p: 'lorem20' },
-		{ date: '05/06/2022', h2: 'test right text', p: 'lorem20' },
-		{ date: '07/09/2022', h2: 'test left text', p: 'lorem20' },
-		{ date: '15/12/2022', h2: 'test right text', p: 'lorem20' }
+		{ isOpen :false, date: '01/04/2022', h2: 'test left text', p: 'lorem20' },
+		{ isOpen :false, date: '05/06/2022', h2: 'test right text', p: 'lorem20' },
+		{ isOpen :false, date: '07/09/2022', h2: 'test left text', p: 'lorem20' },
+		{ isOpen :false, date: '15/12/2022', h2: 'test right text', p: 'lorem20' }
 	];
+	
 </script>
 
 <div class="timeline">
@@ -13,16 +15,20 @@
 			<div class="container left">
 				<div class="content">
 					<div class="Ldate">{event.date}</div>
-					<h2>{event.h2}</h2>
-					<p>{event.p}</p>
+					<h2 class="h2"  on:click={() => (event.isOpen = !event.isOpen)}>{event.h2}</h2>
+					{#if event.isOpen}
+					<p class="p" in:fade out:fade>{event.p}</p>
+					{/if}
 				</div>
 			</div>
 		{:else}
 			<div class="container right">
 				<div class="content">
 					<div class="Rdate">{event.date}</div>
-					<h2>{event.h2}</h2>
-					<p>{event.p}</p>
+					<h2 class="h2"  on:click={() => (event.isOpen = !event.isOpen)}>{event.h2}</h2>
+					{#if event.isOpen}
+					<p class="p" in:fade out:fade>{event.p}</p>
+					{/if}
 				</div>
 			</div>
 		{/if}
@@ -30,11 +36,16 @@
 </div>
 
 <style lang="scss">
+	$break: 615px;
+	
 	.timeline {
 		width: 100vw;
 		background-color: rgb(162, 182, 143);
 		display: grid;
 		position: relative;
+		@media only screen and (max-width: $break) {
+			background-color: rgb(162, 152, 163);
+		}
 	}
 	.timeline::after {
 		content: '';
@@ -45,43 +56,60 @@
 		bottom: 0;
 		left: 50%;
 		margin-left: -3px;
+		@media only screen and (max-width: $break) {
+			left :10%
+		}
 	}
 	.left {
 		display: grid;
 		grid-template-areas: 'content .';
 		grid-template-columns: 50vw 50vw;
 		grid-template-rows: 50vh;
+		@media only screen and (max-width: $break) {
+			grid-template-areas: 'content';
+			grid-template-columns: 50vw;
+			grid-template-rows: 50vh;
+		}
 	}
 
 	.Ldate {
 		display: grid;
-		min-width:  calc(10vw - 3px);
+		width: calc(15vw - 3px);
 		position: absolute;
 		top: -1vh;
-		right: calc(-10vw - 6px);
+		right: calc(-15vw - 6px);
 		z-index: 1;
 		background-color: beige;
-		padding: 3px;		
-		border-radius: 5%;
+		padding: 3px;
+		border-radius: 10px;
 		justify-items: center;
+		@media only screen and (max-width: $break) {
+			top: -1vh;
+		    right: calc(80vw + 6px);
+		}
 	}
 	.right {
 		display: grid;
 		grid-template-areas: '. content';
 		grid-template-columns: 50vw 50vw;
 		grid-template-rows: 50vh;
+		@media only screen and (max-width: $break) {
+			grid-template-areas: 'content';
+			grid-template-columns: 50vw;
+			grid-template-rows: 50vh;
+		}
 	}
 	.Rdate {
 		display: grid;
-		min-width: calc(10vw - 3px);
+		width: calc(15vw - 3px);
 		position: absolute;
 		top: -1vh;
-		left: calc(-10vw - 6px);
+		left: calc(-15vw - 6px);
 		justify-items: center;
 		z-index: 1;
 		background-color: beige;
 		padding: 3px;
-		border-radius: 5%;
+		border-radius: 10px;
 	}
 	.container {
 		display: grid;
@@ -90,6 +118,11 @@
 		background-color: cornflowerblue;
 		justify-items: center;
 		align-items: center;
+		@media only screen and (max-width: $break) {
+			grid-template-columns: 100vw;
+			justify-items: end;
+			
+		}
 	}
 	.content {
 		display: grid;
@@ -97,6 +130,52 @@
 		height: 25vmin;
 		position: relative;
 		grid-area: content;
-		background-color: darkcyan;
+		@media only screen and (max-width: $break) {
+			width: 80vw;
+			justify-items: end;
+			
+		}
+		
+		
+	}
+	.h2{
+		display: grid;
+		cursor: pointer;
+		height: 10vh;
+		align-items: center;
+		background-color: white;
+		padding-left:3px;
+		border-radius: 10px;
+		
+		position:relative;
+		z-index:1;
+		transition: border-radius 4s;
+		&:hover {
+			border-radius:10px 10px 0 0;		
+			}
+		@media only screen and (max-width: $break) {
+			width: 80vw;
+		}
+		
+	}
+	.p{
+		display: grid;
+		padding:3px;
+		backdrop-filter: blur(16px) saturate(180%);
+    	-webkit-backdrop-filter: blur(16px) saturate(180%);
+    	background-color: rgba(17, 25, 40, 0.75);
+    	border-radius: 12px;
+    	border: 1px solid rgba(255, 255, 255, 0.125);
+		align-items: center;
+		height:15vmin;
+		width:40vw;
+		border-radius: 0 0 10px 10px ;
+		color: white;
+		position:absolute;
+		bottom: 10px;
+		left:0;
+		@media only screen and (max-width: $break) {
+			width: 80vw;
+		}
 	}
 </style>
